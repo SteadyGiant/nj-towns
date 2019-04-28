846e2e0aeb4bd47429bae0a0a8ef90ac32bbef16
everetr <everet.rummel@gmail.com>
2019-04-27 21:27:02 -0400

    function not needed

--- code/R/best_towns_get.R
+++ code/R/best_towns_get.R
@@ -19,16 +19,6 @@ library(dplyr)
 library(readr)
 library(rvest)
 
-# ?toupper
-simpleCap =
-  Vectorize({
-    function(x) {
-      s <- strsplit(x, " ")[[1]]
-      paste(toupper(substring(s, 1, 1)), substring(s, 2),
-            sep = "", collapse = " ")
-    }
-  })
-
 
 ##############
 ### Import ###
