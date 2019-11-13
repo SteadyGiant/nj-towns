cd13387966bf940623e47c142038d461b6b56469
everetr <2319774-everetr@users.noreply.gitlab.com>
2019-11-13 13:17:13 -0500

    Keep ACS names. Join GEOIDs to newly included towns w/ pop >= 1k.

--- code/crime.R
+++ code/crime.R
@@ -10,7 +10,7 @@ ids =
   read_csv('./data/output/diversity_econ.csv') %>%
   select(GEOID, municipality, county) %>%
   mutate(
-    municipality = municipality %>%
+    join_col = municipality %>%
       str_replace_all(
         c(' city$| borough$| town$| village$| Village township$' = '',
           'township' = 'Township',
@@ -22,18 +22,29 @@ ids =
           'Verona Township' = 'Verona',
           'West Orange Township' = 'West Orange')
       ),
-    municipality = case_when(
+    join_col = case_when(
+      GEOID == '3401322385' ~ 'Fairfield Township, Essex County',
       GEOID == '3401524840' ~ 'Franklin Township, Gloucester County',
+      GEOID == '3401924870' ~ 'Franklin Township, Hunterdon County',
       GEOID == '3403524900' ~ 'Franklin Township, Somerset County',
+      GEOID == '3401528185' ~ 'Greenwich Township, Gloucester County',
+      GEOID == '3404128260' ~ 'Greenwich Township, Warren County',
       GEOID == '3400129280' ~ 'Hamilton Township, Atlantic County',
       GEOID == '3402129310' ~ 'Hamilton Township, Mercer County',
       GEOID == '3402139510' ~ 'Lawrence Township, Mercer County',
+      GEOID == '3400543290' ~ 'Mansfield Township, Burlington County',
+      GEOID == '3404143320' ~ 'Mansfield Township, Warren County',
       GEOID == '3402347280' ~ 'Monroe Township, Gloucester County',
       GEOID == '3401547250' ~ 'Monroe Township, Middlesex County',
       GEOID == '3402554270' ~ 'Ocean Township, Monmouth County',
+      GEOID == '3402954300' ~ 'Ocean Township, Ocean County',
+      GEOID == '3400569990' ~ 'Springfield Township, Burlington County',
+      GEOID == '3403970020' ~ 'Springfield Township, Union County',
+      GEOID == '3400377135' ~ 'Washington Township, Bergen County',
       GEOID == '3401577180' ~ 'Washington Township, Gloucester County',
       GEOID == '3402777240' ~ 'Washington Township, Morris County',
-      TRUE ~ municipality
+      GEOID == '3404177300' ~ 'Washington Township, Warren County',
+      TRUE ~ join_col
     )
   )
 
@@ -46,53 +57,65 @@ data_in = cius %>%
   mutate(
     city = city %>%
       str_replace_all(
-          # typos
+        # typos
         c('Burlinton' = 'Burlington', # 2007
           'Glouchester' = 'Gloucester', # 2010
+          ' Burough' = '', # Hopewell, 2017
+          # wrong
+          "Carney's Point Township" = 'Carneys Point Township',
           # Standardize names. Some changed in later years' files.
           ' Borough| Village| County Police Department|[0-9]' = '',
+          'Bordentown City' = 'Bordentown',
+          'Burlington City' = 'Burlington',
           'Monroe Township$' = 'Monroe Township, Gloucester County',
           'Orange City' = 'Orange',
-          'Springfield Township, Union County|^Springfield$' = 'Springfield Township',
+          'Peapack-Gladstone' = 'Peapack and Gladstone',
           'Washington Township, Mercer County' = 'Robbinsville Township',
           'West Paterson' = 'Woodland Park')
-      )
-  ) %>%
-  rename(municipality = city)
+      ),
+    # Springfield Townships are particularly annoying
+    city = case_when(
+      grepl('Springfield', city)
+      & population > 10000 ~ 'Springfield Township, Union County',
+      grepl('Springfield', city)
+      & population < 10000 ~ 'Springfield Township, Burlington County',
+      TRUE ~ city
+    )
+  )
 
 # Princeton Borough absorbed Princeton Township after 2010.
 # Combine their pops & crimes in 2010 data.
 princetons = data_in %>%
-  filter(grepl('Princeton', municipality)) %>%
+  filter(grepl('Princeton', city)) %>%
   group_by(year) %>%
-  summarize_at(.vars = vars(-municipality),
+  summarize_at(.vars = vars(-city),
                ~ sum(.)) %>%
   ungroup() %>%
-  mutate(municipality = 'Princeton')
+  mutate(city = 'Princeton')
 
 data_cln = data_in %>%
-  filter(!grepl('Princeton', municipality)) %>%
+  filter(!grepl('Princeton', city)) %>%
   bind_rows(princetons) %>%
-  select(municipality, year, everything()) %>%
-  arrange(municipality, year) %>%
+  select(city, year, everything()) %>%
+  arrange(city, year) %>%
   group_by(year)
 
 rm(princetons)
 
 # Newark missing data for 2015
 # data_cln %>%
-#   count(municipality) %>%
+#   count(city) %>%
 #   filter(n != 13) %>%
 #   View()
 
 # median pop over all years
-median(data_cln$population)
+# median(data_cln$population)
 # [1] 9594.5
 
 # Universe:
 #   - At least 1k residents
 data_uni = data_cln %>%
-  group_by(municipality) %>%
+  group_by(city) %>%
   filter(all(population >= 1000)) %>%
   ungroup()
 
@@ -102,7 +125,7 @@ data_uni = data_cln %>%
 # https://www.pewresearch.org/fact-tank/2019/10/17/facts-about-crime-in-the-u-s/
 data_out = data_uni %>%
   # group for calculting pct changes
-  group_by(municipality) %>%
+  group_by(city) %>%
   mutate(
     violent_per1k = (violent_crime / population) * 1000,
     violent_per1k_pct_chg = (violent_per1k / lag(violent_per1k)) - 1,
@@ -116,7 +139,10 @@ data_out = data_uni %>%
     state_property_crime_per1k =
       (sum(property_crime, na.rm = TRUE) / population) * 1000
   ) %>%
-  left_join(ids, by = 'municipality') %>%
+  left_join(ids, by = c('city' = 'join_col')) %>%
+  select(-city) %>%
   select(GEOID, municipality, county, year, everything())
+
+# Ship Bottom has no counterpart in ACS(?). Oh well.
 
 write_csv(data_out, path = './data/output/crime.csv')
