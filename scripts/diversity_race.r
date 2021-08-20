d76a9708abf26480b050d17448cc15f1448fdfa3
everet <everet@disroot.org>
2021-08-20 10:00:55 -0400

    Calculate statewide diversity. Combine all data into 1 file. Greater population restriction.

--- scripts/diversity_race.r
+++ scripts/diversity_race.r
@@ -7,26 +7,28 @@ library(PL94171)
 library(readr)
 library(tigris)
 
-# Get county subdivision (municipality) names.
-nj_cosubd = tigris::county_subdivisions("NJ", year = 2020) %>%
-  sf::st_drop_geometry() %>%
-  dplyr::select(GEOID, municipality = NAME)
+# Get decennial data for county subdivisions.
+# https://www2.census.gov/programs-surveys/decennial/2020/data/01-Redistricting_File--PL_94-171/New_Jersey/
+# NOTE: The warning message is wrong. 2020 PL files have been released.
+pl = PL94171::pl_url("NJ", 2020) %>%
+  PL94171::pl_read() %>%
+  # print(PL94171::pl_geog_levels)
+  PL94171::pl_subset(sumlev = "060") %>%
+  PL94171::pl_select_standard(clean_names = TRUE)
+
 # Get county names.
 nj_co = tigris::counties("NJ", year = 2020) %>%
   sf::st_drop_geometry() %>%
-  dplyr::select(COUNTYFP, county = NAME)
+  dplyr::select(FIPS = COUNTYFP, county = NAME)
+# Get county subdivision (municipality) names.
+nj_cosub = tigris::county_subdivisions("NJ", year = 2020) %>%
+  sf::st_drop_geometry() %>%
+  dplyr::select(GEOID, municipality = NAME)
 
-# Get decennial data for county subdivisions.
-# https://www2.census.gov/programs-surveys/decennial/2020/data/01-Redistricting_File--PL_94-171/New_Jersey/
-pl = PL94171::pl_url("NJ", 2020) |>
-  PL94171::pl_read() |>
-  # print(PL94171::pl_geog_levels)
-  PL94171::pl_subset(sumlev = "060")|>
-  PL94171::pl_select_standard(clean_names = TRUE)
 
-calc = function(dat, front_cols = NULL) {
+# Get group proportions, diversity index, rankings, & geography stats.
+calc = function(dat) {
   dat %>%
-    dplyr::filter(pop > 0) %>%
     dplyr::mutate(
       dplyr::across(pop_hisp:pop_two, `/`, pop, .names = "pct_{.col}")
     ) %>%
@@ -36,31 +38,50 @@ calc = function(dat, front_cols = NULL) {
       diversity = 1 - sum(dplyr::c_across(dplyr::starts_with("pct_"))^2)
     ) %>%
     dplyr::ungroup() %>%
-    dplyr::mutate(diversity_rank = dplyr::min_rank(dplyr::desc(diversity))) %>%
-    dplyr::arrange(diversity_rank) %>%
-    dplyr::rename(county_fips = county) %>%
-    dplyr::left_join(nj_co, by = c("county_fips" = "COUNTYFP")) %>%
-    dplyr::select(
-      dplyr::all_of(front_cols),
-      county,
-      county_fips,
-      diversity,
-      diversity_rank,
-      dplyr::starts_with("pct"),
-      dplyr::starts_with("pop")
-    )
+    dplyr::mutate(
+      diversity_rank = dplyr::min_rank(dplyr::desc(diversity)),
+      median_diversity = median(diversity)
+    ) %>%
+    dplyr::arrange(diversity_rank)
 }
 
+
 muni = pl %>%
-  calc("GEOID") %>%
-  dplyr::left_join(nj_cosubd, by = "GEOID") %>%
-  dplyr::relocate(municipality)
-counties = pl %>%
-  dplyr::group_by(county) %>%
+  # Someone else made this decision for me:
+  # https://www.nj.com/data/2019/02/the-25-most-racially-diverse-towns-in-nj-ranked.html
+  dplyr::filter(pop > 10000) %>%
+  calc() %>%
+  dplyr::rename(FIPS = county) %>%
+  # Add municipality names.
+  dplyr::left_join(nj_cosub, by = "GEOID") %>%
+  # Add county names.
+  dplyr::left_join(nj_co, by = "FIPS") %>%
+  dplyr::rename(name = municipality) %>%
+  dplyr::mutate(level = "municipality")
+counties = muni %>%
+  dplyr::group_by(name = county, FIPS) %>%
   dplyr::summarise(dplyr::across(dplyr::starts_with("pop"), sum)) %>%
   dplyr::ungroup() %>%
   calc() %>%
-  dplyr::rename(FIPS = county_fips)
+  dplyr::mutate(level = "county")
+state = counties %>%
+  dplyr::summarise(dplyr::across(dplyr::starts_with("pop"), sum)) %>%
+  calc() %>%
+  dplyr::select(-diversity_rank) %>%
+  dplyr::mutate(level = "state", name = "New Jersey")
+data_out = dplyr::bind_rows(muni, counties, state) %>%
+  dplyr::select(
+    level,
+    name,
+    GEOID,
+    county,
+    FIPS,
+    diversity_rank,
+    diversity,
+    median_diversity,
+    dplyr::starts_with("pct"),
+    pop,
+    dplyr::starts_with("pop_")
+  )
 
-readr::write_csv(muni, "data/diversity_race_municipality.csv")
-readr::write_csv(counties, "data/diversity_race_county.csv")
+readr::write_csv(data_out, "data/diversity_race.csv")
